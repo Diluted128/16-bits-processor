5e7b65e403d37f91632ec5f775bfca301aa43d9f
Diluted666 <jacoszekwojciech@gmail.com>
2021-05-20 22:52:47 +0200

    Modified ControlBlock

--- ControlBlock.vhd
+++ ControlBlock.vhd
@@ -23,7 +23,7 @@ end entity;
  
 architecture rtl of control is
 type state_type is (m0, m1, m10, m11, m12, m13, m14, m15, m16, m17, m20, m21, m22, m23, m24, m25, m26, m27, m28,
-m29, m30, m31, m32, m33, m34, m35, m36, m37, m38, m40, m41, m50, m60, m61, m62, m63, m64, m65, m66, m67, m68,
+m29, m30, m31, m32, m33, m34, m35, m36, m37, m38, m40, m41, m50, m51, m52, m53, m60, m61, m62, m63, m64, m65, m66, m67, m68,
 m69, m70, m80, m81, m82, m83, m84, m9, m91, m92, m93, m94);
 signal state : state_type;
 begin
@@ -112,17 +112,69 @@ process (clk, reset)
 				----------------------------- M1 Koniec rozkodowywania sygnalu
                end case;
             when m10=>
-              if INT = '1' then state <= m9;
-              else state <= m10;
+              if INT = '0' then state <= m10;
+              else state <= m9;
               end if;
             when m11 => state <= m12;
             when m12 => state <= m13;
             when m13 => state <= m14;
             when m14 =>
-              if INT = '1' then state <= m9;
-              else state <= m0;
+              if INT = '0' then state <= m0;
+              else state <= m9;
               end if;
-          -- ... ---------------------------    <- dodac
+            when m15 => state <= m16;
+				when m16 => state <= m17;
+				when m17 => 
+				  if INT = '0' then state <= m0;
+              else state <= m9;
+              end if;
+				when m20 =>
+				  if INT = '0' then state <= m0;
+              else state <= m9;
+              end if;
+				when m21 => state <= m22;
+				when m22 =>
+				  if INT = '0' then state <= m0;
+              else state <= m9;
+              end if;
+				when m23 =>
+				  if INT = '0' then state <= m0;
+              else state <= m9;
+              end if;
+				when m24 =>
+				when m25 =>
+				when m26 =>
+				when m27 =>
+				when m28 =>
+				when m29 =>
+				when m30 =>                           --      <- dodac, co bedzie sie dzialo po wykonaniu obliczen na alu.
+				when m31 =>
+				when m32 =>
+				when m33 =>
+				when m34 =>
+				when m35 =>
+				when m36 =>
+				when m37 =>
+				when m38 =>
+				when m40 => 
+				  if (IR(12 downto 11) = "00") then state <= m41;
+				  elsif ( IR(12 downto 11) = "01" and C = '1') then state <= m41;
+				  elsif ( IR(12 downto 11) = "10" and Z = '1') then state <= m41;
+				  elsif ( IR(12 downto 11) = "11" and S = '1') then state <= m41;
+				  elsif ( INT = '0') then state <= m9;
+				  else state <= m0;
+				  end if;
+				when m41 => 
+				  if INT = '0' then state <= m0;
+              else state <= m9;
+				  end if;
+				when m50 => 
+				  if INT = '0' then state <= m0;
+              else state <= m9;
+				  end if;
+				when m60 =>
+				when m80 =>                           --     <- dodać, co bedzie sie dzialo po wykonaniu rozkazu dwuargumentowego.
+				
         end case;
      end if;
 end process;
@@ -194,7 +246,7 @@ begin
 			-- Wspomaganie pamięci: wysyłamy wartosc wskaznika stosu na AD
 			-- Zapis z D do DI a nastepnie do zapisujemy DI do rejestru określonego w IR
          when m20 =>
-		     Sa <= "10"; Sbb <= "0000"; Sba <= "0" & IR(3 downto 0); Sid<="010"; MIO <='1';
+		   --  Sa <= "10"; Sbb <= "0000"; Sba <= "0" & IR(3 downto 0); Sid<="010"; MIO <='1';                      <- problem bo wektor bedzie mial 5 bitow a Sba jest 4 bitowy
 			  Smar <='1'; Smbr <='0'; WR <='0'; RD <='1'; Salu <="0000";  
 			------------------------------------------------------------------------------------
 			-- Rozkaz POP R
@@ -202,14 +254,14 @@ begin
 			-- Wspomaganie pamięci: przesylamy wartosc SP na AD
 			-- Wspomaganie pamięci: IR(3 downto 0) <- decyzja ktora wartosc z rejestru zwolnic (wyslac na zatrzask MBR(MBR(out))
 			when m21 =>
-		     Sa <= "10"; Sbb := "0" & IR(3 downto 0); Sid<="011"; MIO <='1';                
+		   --  Sa <= "10"; Sbb := "0" & IR(3 downto 0); Sid<="011"; MIO <='1';                
 			  Smar <='1'; Smbr <= '1'; WR <='0'; RD <='0'; Salu <="0000"; 
 			------------------------------------------------------------------------------------
 			-- Rozkaz NEG R
 			-- Wybieramy ktora wartosc rejestru chcemy poddać negacji poprzez Sbb
 			-- Zapisujemy otrzymana wartosc do tego samego rejestru z ktorego wczesniej pobralismy wartosc.
 			when m23 =>
-		     Sbb <= "0" & IR(3 downto 0); Sba <= "0" & IR(3 downto 0); MIO <='0';
+		   --  Sbb <= "0" & IR(3 downto 0); Sba <= "0" & IR(3 downto 0); MIO <='0';
 			  Salu <="1001"; 
 			------------------------------------------------------------------------------------
 			-- INC R
@@ -235,7 +287,7 @@ begin
 			------------------------------------------------------------------------------------
 			-- ADD R,RM
 			when m31 =>
-			------------------------------------------------------------------------------------
+			------------------------------------------------------------------------------------      <- dodac, opisac
 			-- SUB R,RM
 			when m32 =>
 			------------------------------------------------------------------------------------
@@ -257,17 +309,22 @@ begin
 			-- OUT IO(AD),R
 			when m38 =>
 			------------------------------------------------------------------------------------
-			-- Rozkazy skoków
+			-- Rozkazy skoków cz1
 			when m40 =>
-		     Sa <= "01"; Sbb <= "0000"; Sba <= "0001"; Sid<="001"; Sbc<="0000"; MIO <='1';
-			  Smar <='1'; Smbr <= '0'; WR <='0'; RD <='1'; Salu <="0000"; LDF <='0'; INTA <='0';
+		     Sa <= "01"; Sbb <= "0000"; Sba <= "0001"; Sid<="001"; MIO <='1';
+			  Smar <='1'; Smbr <= '0'; WR <='0'; RD <='1'; Salu <="0000"; 
+			------------------------------------------------------------------------------------     <- opisac
+			-- Rozkazy skoków cz2
 			when m41 =>
-			  Sa <= "00"; Sbb <= "1010"; Sba <= "0001"; Sid<="000"; Sbc<="0001"; MIO <='0';
-			  Smar <='0'; Smbr <= '0'; WR <='0'; RD <='0'; Salu <="0010"; LDF <='0'; INTA <='0';
+			  Sbb <= "1010"; Sba <= "0001"; Sbc<="0001"; MIO <='0';
+			  Smar <='0'; Smbr <= '0'; WR <='0'; RD <='0'; Salu <="0010";
 			------------------------------------------------------------------------------------
 			-- Rozkaz skoku długiego
 			when m50 =>
-			------------------------------------------------------------------------------------
+			when m51 =>                                                                     
+	      when m52 =>
+	      when m53 =>		
+			------------------------------------------------------------------------------------     <- dodac, opisac 
 			-- Rozkazy dwuargumentowe
 			when m60 =>
 			------------------------------------------------------------------------------------
